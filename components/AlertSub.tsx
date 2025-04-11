53302776f9a0a4cad880d6fa495fa968d8107509
NayanCod <kumarnayab6@gmail.com>
2025-04-11 13:10:55 +0530

    latest push with payment and subscription

--- components/AlertSub.tsx
+++ components/AlertSub.tsx
@@ -9,19 +9,22 @@ const AlertSub = () => {
   return (
     <>
       <div
-        className={`fixed top-0 w-full bg-primary-200 flex flex-row justify-between items-center px-6 ${
+        className={`bg-primary-200 flex flex-row justify-between items-center md:px-6 px-2 ${
           show ? "" : "hidden"
         }`}
       >
-        <p className="text-dark-200 text-sm font-semibold">
+        <p className="text-dark-200 text-sm font-semibold md:block hidden">
           Get unlimited voice based mock interviews with ai assitent with Pro
           subscription
+        </p>
+        <p className="text-dark-200 text-sm font-semibold block md:hidden">
+          Get unlimited voice interviews with Pro
         </p>
         <Button
           className="bg-transparent hover:bg-transparent text-dark-200 cursor-pointer font-bold"
           onClick={() => setShow(false)}
         >
-          <X strokeWidth={3} className="font-bold" />
+          <X strokeWidth={3} />
         </Button>
       </div>
     </>
