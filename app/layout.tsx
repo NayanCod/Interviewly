a42a89aa90133f0656b5d5b63caee9883680c6f3
NayanCod <kumarnayab6@gmail.com>
2025-04-21 16:27:02 +0530

    add pwa implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
+  manifest: "/manifest.json",
   title: "Interviewly",
   description: "An AI-powered platform for preparing for mock interviews",
 };
--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 
 const nextConfig: NextConfig = {
@@ -10,4 +11,12 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: false,
+  reloadOnOnline: true
+}
+
+export default withPWA(pwaConfig)(nextConfig as any);
